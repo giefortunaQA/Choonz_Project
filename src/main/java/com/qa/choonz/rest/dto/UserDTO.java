11e3ac1c35b6c6aa01ee9c06a9182d787d7db685
CGuthrieQA <CGuthrie@QA.com>
2021-03-15 15:22:14 +0000

    CGP-58 #CG lombok refactor of user dto

--- src/main/java/com/qa/choonz/rest/dto/UserDTO.java
+++ src/main/java/com/qa/choonz/rest/dto/UserDTO.java
@@ -4,110 +4,18 @@ import java.util.List;
 
 import com.qa.choonz.persistence.domain.Playlist;
 
-public class UserDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-	// variables
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
 
 	private Long id;
 	private String username;
 	private String password;
 	private List<Playlist> playlists;
-
-	// constructors
-
-	public UserDTO(Long id, String username, String password, List<Playlist> playlists) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.playlists = playlists;
-	}
-
-	// getters and setters
-
-	public UserDTO() {
-		super();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public List<Playlist> getPlaylists() {
-		return playlists;
-	}
-
-	public void setPlaylists(List<Playlist> playlists) {
-		this.playlists = playlists;
-	}
-
-	@Override
-	public String toString() {
-		return "UserDTO [id=" + id + ", username=" + username + ", password=" + password + ", playlists=" + playlists
-				+ "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((playlists == null) ? 0 : playlists.hashCode());
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UserDTO other = (UserDTO) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (playlists == null) {
-			if (other.playlists != null)
-				return false;
-		} else if (!playlists.equals(other.playlists))
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
-			return false;
-		return true;
-	}
 
 }
