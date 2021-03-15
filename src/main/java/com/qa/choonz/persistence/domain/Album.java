1617e470429b0e7b5504797677e86bf2ac480084
CGuthrieQA <CGuthrie@QA.com>
2021-03-15 15:49:46 +0000

    CGP-58 #CG lombok refactor of album domain

--- src/main/java/com/qa/choonz/persistence/domain/Album.java
+++ src/main/java/com/qa/choonz/persistence/domain/Album.java
@@ -16,6 +16,13 @@ import javax.validation.constraints.Size;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Album {
 
@@ -40,11 +47,6 @@ public class Album {
 
 	private String cover;
 
-	public Album() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
 	public Album(long id, @NotNull @Size(max = 100) String name, Artist artist, Genre genre, String cover) {
 		super();
 		this.id = id;
@@ -52,100 +54,6 @@ public class Album {
 		this.artist = artist;
 		this.genre = genre;
 		this.cover = cover;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Artist getArtist() {
-		return artist;
-	}
-
-	public void setArtist(Artist artist) {
-		this.artist = artist;
-	}
-
-	public Genre getGenre() {
-		return genre;
-	}
-
-	public void setGenre(Genre genre) {
-		this.genre = genre;
-	}
-
-	public String getCover() {
-		return cover;
-	}
-
-	public void setCover(String cover) {
-		this.cover = cover;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Album [id=").append(id).append(", name=").append(name).append(", artist=").append(artist)
-				.append(", genre=").append(genre).append(", cover=").append(cover).append("]");
-		return builder.toString();
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((artist == null) ? 0 : artist.hashCode());
-		result = prime * result + ((cover == null) ? 0 : cover.hashCode());
-		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Album other = (Album) obj;
-		if (artist == null) {
-			if (other.artist != null)
-				return false;
-		} else if (!artist.equals(other.artist))
-			return false;
-		if (cover == null) {
-			if (other.cover != null)
-				return false;
-		} else if (!cover.equals(other.cover))
-			return false;
-		if (genre == null) {
-			if (other.genre != null)
-				return false;
-		} else if (!genre.equals(other.genre))
-			return false;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
 	}
 
 }
