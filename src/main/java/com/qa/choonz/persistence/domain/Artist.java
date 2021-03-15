79d40675a13f1c13c7d6d16258bf009312a5e0b0
CGuthrieQA <CGuthrie@QA.com>
2021-03-15 15:49:04 +0000

    CGP-58 #CG lombok refactor of artist domain

--- src/main/java/com/qa/choonz/persistence/domain/Artist.java
+++ src/main/java/com/qa/choonz/persistence/domain/Artist.java
@@ -12,6 +12,13 @@ import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Artist {
 
@@ -27,66 +34,10 @@ public class Artist {
 	@OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
 	private List<Album> albums;
 
-	public Artist() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
 	public Artist(long id, @NotNull @Size(max = 100) String name) {
 		super();
 		this.id = id;
 		this.name = name;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Artist [id=").append(id).append(", name=").append(name).append("]");
-		return builder.toString();
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Artist other = (Artist) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
 	}
 
 }
