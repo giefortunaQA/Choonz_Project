8144e63a2bf520d5d8ffff79ed3c8e1d357f605c
Gie-Anne Fortuna <gfortuna@qa.com>
2021-03-16 11:45:00 +0000

    Added all service tests. Switching branches to work on recursion fix

---
+++ src/test/java/com/qa/choonz/service/UserServiceTest.java
@@ -0,0 +1,90 @@
+package com.qa.choonz.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.qa.choonz.persistence.domain.User;
+import com.qa.choonz.persistence.repository.UserRepository;
+import com.qa.choonz.rest.dto.UserDTO;
+
+
+@SpringBootTest
+@ActiveProfiles	("test")
+public class UserServiceTest {
+
+	@Autowired
+	private UserService service;
+	
+	@MockBean
+	private UserRepository repo;
+	
+	private ModelMapper mapper=new ModelMapper();
+	
+	private UserDTO mapToDTO(User user) {
+		return this.mapper.map(user,UserDTO.class);
+	}
+	//class resources
+	private final User testUser1=new User("User 1","pass 1");
+	private final User testUser2=new User("User 2","pass 2");
+	private final List<User> testList=List.of(testUser1,testUser2); 
+	
+	@Test
+	void testCreate() throws Exception{
+		User toCreate=new User("User","pass");
+		User created=new User(5L,"User","pass");
+		
+		when(this.repo.save(toCreate)).thenReturn(created);
+		
+		assertThat(this.service.create(toCreate)).isEqualTo(this.mapToDTO(created));
+		verify(this.repo,times(1)).save(toCreate);
+	}
+	
+	@Test
+	void testReadAll() throws Exception{
+		List<UserDTO> testListAsDtos=testList.stream().map(this::mapToDTO).collect(Collectors.toList());
+		when(this.repo.findAll()).thenReturn(testList);
+		assertThat(this.service.read()).isEqualTo(testListAsDtos);
+	}
+	
+	@Test
+	void testReadById() throws Exception{
+		Long id=1L;
+		when(this.repo.findById(id)).thenReturn(Optional.of(testUser1));
+		assertThat(this.service.read(id)).isEqualTo(this.mapToDTO(testUser1));
+		verify(this.repo,times(1)).findById(id);
+	}
+	
+	@Test
+	void testUpdate() throws Exception{
+		Long id=1L;
+		User updated=new User(1L,"User 1 Updated","pass 1 updated");
+		UserDTO updatedAsDto=this.mapToDTO(updated);
+		when(this.repo.findById(id)).thenReturn(Optional.of(testUser1));
+		when(this.repo.save(updated)).thenReturn(updated);
+		assertThat(this.service.update(updated, id)).isEqualTo(updatedAsDto);
+		verify(this.repo,times(1)).findById(id);
+		verify(this.repo,times(1)).save(updated);
+	}
+	
+	@Test
+	void testDeleteFail() throws Exception{
+		Long id=1L;
+		when(this.repo.existsById(id)).thenReturn(true);
+		assertThat(this.service.delete(id)).isFalse();
+		verify(this.repo,times(1)).existsById(id);
+	}
+	
+}
