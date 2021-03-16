0d72896f7d9573b946ed15a94b9f589b8d68ea17
CGuthrieQA <CGuthrie@QA.com>
2021-03-16 14:03:55 +0000

    CGP-48 #CG created CukeRunner

--- src/test/java/com/qa/choonz/pom/cuke/CukeRunner.java
+++ src/test/java/com/qa/choonz/pom/cuke/CukeRunner.java
@@ -1,5 +1,18 @@
 package com.qa.choonz.pom.cuke;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/cuke",
+		tags = "not @ignore",
+		stepNotifications = true,
+		glue = "cucumber.stepdefs",
+		plugin = {"pretty", "html:target/cucumber-reports.html"},
+		monochrome = true)
 public class CukeRunner {
 
 }
