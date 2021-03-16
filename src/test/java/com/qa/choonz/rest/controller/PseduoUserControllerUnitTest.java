791d78148b579700390cc8e6742bdd2cb517206c
SehunB-QA <sbabatunde@qa.com>
2021-03-16 00:29:08 +0000

    CGP-54 #SB Added more pseudocode to the pseudo user controller's add method. Did some refactoring on the pseudoUserController class for better naming clarity and others. Added a read all users test to the UserController UnitTest

--- src/test/java/com/qa/choonz/rest/controller/PseduoUserControllerUnitTest.java
+++ src/test/java/com/qa/choonz/rest/controller/PseduoUserControllerUnitTest.java
@@ -1,19 +1,37 @@
 package com.qa.choonz.rest.controller;
 
-import org.junit.jupiter.api.Test;
 
 public class PseduoUserControllerUnitTest {
 	
 	
-	@Test
-	public void create()
+
+	public void pseudoCreateTest()
 	{
-		//RESOURCES
+						  // RESOURCES \\
 		
 		//New User Object (username,password)
 		//To store our user values
 		
-		//new UserDTO object
+		//MapToDTO Method
+		// A method used to convert a User entity into a Data transfer object (UserDTO)
+		
+							 // ACTIONS \\
+		
+		//Mock when a userService invokes it's create method with the new User object as it's parameter
+		//Return a UserDTO object that has had an User entity mapped to it
+		
+		
+						    // ASSERTIONS \\
+		
+		//Confirm that the a new responseEntiy object is made of a UserDTo object
+		//Which contains a response code of 201
+		//And that it is equal to of how our actual controller will perform the create method 
+		//using our user object 
+		
+		//assertThat(new ResponseEntity<>(this.mapToDTO(), HttpStatus.CREATED))
+		//.isEqualTo(this.controller.create());
+		
+		//Verify the userService create method has been executed at least once 
 		
 	}
 	
--- src/test/java/com/qa/choonz/rest/controller/UserControllerUnitTest.java
+++ src/test/java/com/qa/choonz/rest/controller/UserControllerUnitTest.java
@@ -1,10 +1,13 @@
 package com.qa.choonz.rest.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -18,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import com.qa.choonz.persistence.domain.User;
 import com.qa.choonz.rest.dto.UserDTO;
 import com.qa.choonz.service.UserService;
+
 
 @SpringBootTest
 public class UserControllerUnitTest {
@@ -34,7 +38,7 @@ public class UserControllerUnitTest {
 		    private ModelMapper mapper;
 			
 		    private final User testUser = new User(2L, "Sehun",  "Password", null);
-		    private final List<User> listsOfUsers = List.of(testUser);
+		    private final List<User> listOfUsers = List.of(testUser);
 		    
 		    private UserDTO mapToDTO(User user)
 		    {
@@ -45,17 +49,61 @@ public class UserControllerUnitTest {
 	@Test
 	public void createUserTest()
 	{
-
+                             	//ACTIONS
 		Mockito.when(this.userService.create(testUser)).thenReturn(this.mapToDTO(testUser));
 		
-		// THIS
+								
+		                       //ASSERTIONS
+		
 		assertThat(new ResponseEntity<UserDTO>(this.mapToDTO(testUser), HttpStatus.CREATED))
 		.isEqualTo(this.controller.create(testUser));
 			
-		//System.out.println("Expected:" + testUser);
-		//System.out.println("Result:" + controller.create(testUser));
+		
 	    verify(this.userService, atLeastOnce()).create(testUser);
 		
 	}
+	
+	@Test
+	public void readAllUsersTest()
+	{
+		
+							//RESOURCES
+		List<UserDTO> testReadList = listOfUsers.stream().map(this:: mapToDTO).
+				collect(Collectors.toList());
+				
+		
+							  //ACTIONS
+		when(this.userService.read()).thenReturn(testReadList);
+		
+						
+							//ASSERTIONS
+		
+		ResponseEntity<List<UserDTO>> expected = ResponseEntity.ok(testReadList);
+		ResponseEntity<List<UserDTO>> result = this.controller.read();
+		assertEquals(expected, result);
+		
+		
+	}
+	
+	
+
+	@Test
+	public void readUserByIDTest()
+	{
+
+		
+		
+	}
+	
+
+	@Test
+	public void readUserByNameTest()
+	{
+
+	
+		
+	}
+	
+	
 
 }
