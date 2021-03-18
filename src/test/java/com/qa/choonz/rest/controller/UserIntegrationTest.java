06dd061a84403c0cff9a3c40efd10136159c986a
Gie-Anne Fortuna <gfortuna@qa.com>
2021-03-18 09:38:37 +0000

    CGP-47 #GF Introduced use of headers for update and delete user

--- src/test/java/com/qa/choonz/rest/controller/UserIntegrationTest.java
+++ src/test/java/com/qa/choonz/rest/controller/UserIntegrationTest.java
@@ -44,6 +44,7 @@ public class UserIntegrationTest {
 	private String URI="/users";
 	private final UserDTO userAsDto=this.mapToDTO(new User(1L,"admin","admin"));
 	private final List<UserDTO> users=List.of(userAsDto);
+	private final String playlistsJson="[{\"id\":1,\"name\":\"Favourites\",\"description\":\"This is a playlist by admin consisting of public favourites.\",\"artwork\":\"https://icons.iconarchive.com/icons/aha-soft/3d-social/512/Favourites-icon.png\",\"tracks\":[{\"id\":1,\"name\":\"Thank U Next\",\"duration\":207,\"lyrics\":\"Thank U Next lyrics\"},{\"id\":2,\"name\":\"needy\",\"duration\":212,\"lyrics\":\"needy lyrics\"},{\"id\":3,\"name\":\"24K Magic\",\"duration\":240,\"lyrics\":\"24K Magic Lyrics\"},{\"id\":4,\"name\":\"Hello\",\"duration\":220,\"lyrics\":\"Hello lyrics\"},{\"id\":5,\"name\":\"What Do You Mean\",\"duration\":210,\"lyrics\":\"What Do You Mean lyrics\"}]}]}";
 	
 	@Test
 	void testCreate() throws Exception{
@@ -62,28 +63,33 @@ public class UserIntegrationTest {
 	@Test
 	void testReadAll() throws Exception{
 		String artistsJson=this.jsonify.writeValueAsString(users);
+		String expectedString=artistsJson.substring(0,artistsJson.length()-6);
+		expectedString+=playlistsJson+"]";
 		RequestBuilder request=get(URI+"/read");
 		ResultMatcher confirmStatus=status().isOk();
-		ResultMatcher confirmBody=content().json(artistsJson);
+		ResultMatcher confirmBody=content().json(expectedString);
 		this.mvc.perform(request).andExpect(confirmBody).andExpect(confirmStatus);
 	}
 	
 	@Test
 	void testReadById() throws Exception{
 		String ArianaJson=this.jsonify.writeValueAsString(userAsDto);
+		String expectedString=ArianaJson.substring(0,ArianaJson.length()-5);
+		expectedString+=playlistsJson;
 		RequestBuilder request=get(URI+"/read/1");
 		ResultMatcher confirmStatus=status().isOk();
-		ResultMatcher confirmBody=content().json(ArianaJson);
+		ResultMatcher confirmBody=content().json(expectedString);
 		this.mvc.perform(request).andExpect(confirmBody).andExpect(confirmStatus);
 	}
 	
 	@Test
 	void testUpdate() throws Exception{
+		String token = null;
 		UserDTO toUpdate=this.mapToDTO(new User("Pop Update","Description update"));
 		UserDTO expected=this.mapToDTO(new User(1L,"Pop Update","Description update"));
 		String toUpdateJson=this.jsonify.writeValueAsString(toUpdate);
 		String expectedJson=this.jsonify.writeValueAsString(expected);
-		RequestBuilder request=put(URI+"/update/1").contentType(MediaType.APPLICATION_JSON).content(toUpdateJson);
+		RequestBuilder request=put(URI+"/update/1").contentType(MediaType.APPLICATION_JSON).content(toUpdateJson).header("token", token);
 		ResultMatcher confirmStatus=status().isAccepted();
 		ResultMatcher confirmBody=content().json(expectedJson);
 		this.mvc.perform(request).andExpect(confirmBody).andExpect(confirmStatus);
@@ -91,10 +97,20 @@ public class UserIntegrationTest {
 	
 	@Test
 	void testDeletePass() throws Exception{
-		RequestBuilder request=delete(URI+"/delete/2");
+		String token = null;
+		RequestBuilder request=delete(URI+"/delete/1").header("token", token);
 		ResultMatcher confirmStatus=status().isNoContent();
 		ResultMatcher confirmBody=content().string("");
 		this.mvc.perform(request).andExpect(confirmBody).andExpect(confirmStatus);
+	}
+	@Test
+	void testLogin() throws Exception{
+		
+	}
+	
+	@Test 
+	void testLogout() throws Exception{
+		
 	}
 	
 }
